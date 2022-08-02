4a8fb197d47ff0ab95afbed6ca6f70bc610e1db9
hensaX <hendra.sanusi@gmail.com>
2022-08-02 08:25:15 +0700

    finish soal-6

--- soal-6.js
+++ soal-6.js
@@ -2,15 +2,15 @@ const db = require('./app/configs/dbconfig');
 
 
 // fungsi cek exists
-const getBookByAuthorname = async (author_name) => {
+const getBookByCategory = async (category) => {
     let sql = `select 
     b.category_name,c.author_name,c.penname,c.gender,a.*
     from books a 
     left join category b on a.id_category=b.id_category
     left join authors c on a.id_author=c.id_author 
-    where lower(c.author_name) like $author_name `
-    const res = await db.query(sql, { $author_name: `%${author_name.toLowerCase()}%` });
+    where lower(b.category_name) like $category`
+    const res = await db.query(sql, { $category: `%${category.toLowerCase()}%` });
     console.log(res.rows)
 }
 
-getBookByAuthorname('Eka')
+getBookByCategory('komputer')
